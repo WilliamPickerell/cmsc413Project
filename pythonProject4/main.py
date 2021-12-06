b3bb588d5aded23b9fd66ff3f34ee4e870a5046e
WilliamPickerell <66374031+WilliamPickerell@users.noreply.github.com>
2021-12-06 12:32:09 -0500

    conversion finished
    changing the main window is now easier

--- pythonProject4/main.py
+++ pythonProject4/main.py
@@ -30,26 +30,30 @@ class passwordApp(tk.Tk):
         name = self.nameEntry.get()
         password = self.passwordEntry.get()
         if self.login_user(name, password):
-            for child in self.winfo_children():
-                child.destroy()
-            self.title("Password Manager")
-            self.website = tk.Label(self, text="Website:", font="bold").grid(row=0, column=0)
-            self.name = tk.Label(self, text="UserName:", font="bold").grid(row=0, column=1)
-            listOfSites = self.get_websites_names()
-            i = 1
-            for site in listOfSites:
-                tk.Label(self, text=site[0]).grid(row=i, column=0)
-                tk.Label(self, text=site[1]).grid(row=i, column=1)
-                t = i - 1
-                #tk.Button(self, text='View Password', bd='5', command=lambda t=t: show_password(t)).grid(row=i, column=2)
-                i += 1
-            #tk.Button(self, text='Add Password', bd='5', command=add_password).grid(row=i, column=2)
-            self.mainloop()
+            self.write_main_window()
         else:
             view2 = tk.Tk()
             view2.title("Password Manager")
             tk.Label(view2, text="wrong name or password").grid(row=0, column=0)
             view2.mainloop()
+
+    def write_main_window(self):
+        for child in self.winfo_children():
+            child.destroy()
+        self.title("Password Manager")
+        self.website = tk.Label(self, text="Website:", font="bold").grid(row=0, column=0)
+        self.name = tk.Label(self, text="UserName:", font="bold").grid(row=0, column=1)
+        listOfSites = self.get_websites_names()
+        i = 1
+        for site in listOfSites:
+            tk.Label(self, text=site[0]).grid(row=i, column=0)
+            tk.Label(self, text=site[1]).grid(row=i, column=1)
+            t = i - 1
+            tk.Button(self, text='View Password', bd='5', command=lambda t=t: self.show_password(t)).grid(row=i,
+                                                                                                          column=2)
+            i += 1
+        tk.Button(self, text='Add Password', bd='5', command=self.add_password).grid(row=i, column=2)
+        self.mainloop()
 
     def login_user(self, name, password):
         f1 = open("user.txt", "r")
@@ -84,99 +88,98 @@ class passwordApp(tk.Tk):
             listOfSites.append(webName)
         return listOfSites
 
+    def show_password(self, number):
+        password = self.decrypt_password(number)
+        view3 = tk.Tk()
+        view3.title("Password")
+        tk.Label(view3, text=password).grid(row=0, column=0)
+        view3.mainloop()
+
+    def decrypt_password(self, number):
+        encryptedPassword = ""
+        encryptedKey = ""
+        f1 = open("passwords.txt", "r")
+        f2 = open("keys.txt", "r")
+        lines = f1.readlines()
+        i = 0
+        for line in lines:
+            if i == number:
+                parts = line.split(",")
+                end = len(parts[2]) - 2
+                encryptedPassword = parts[2][2:end]
+            i += 1
+        lines = f2.readlines()
+        i = 0
+        for line in lines:
+            if i == number:
+                end = line.__len__() - 2
+                encryptedKey = line[2:end]
+            i += 1
+        keyDecryptor = Fernet(master_key)
+        key = keyDecryptor.decrypt(encryptedKey.encode())
+        passwordDecryptor = Fernet(key)
+        password = passwordDecryptor.decrypt(encryptedPassword.encode()).decode()
+        return password
+
+    def add_password(self):
+        for child in self.winfo_children():
+            child.destroy()
+        self.title("Add Password")
+        tk.Label(self, text="Website:").grid(row=0, column=0)
+        tk.Label(self, text="UserName:").grid(row=1, column=0)
+        tk.Label(self, text="Password:").grid(row=2, column=0)
+        self.websiteEntry = tk.Entry(self)
+        self.websiteEntry.grid(row=0, column=1)
+        self.nameEntry = tk.Entry(self)
+        self.nameEntry.grid(row=1, column=1)
+        self.passwordEntry = tk.Entry(self)
+        self.passwordEntry.grid(row=2, column=1)
+        tk.Button(self, text='confirm', bd='5',
+               command= self.password_confirmation).grid(row=3, column=1)
+        self.mainloop()
+
+    def password_confirmation(self):
+        website = self.websiteEntry.get()
+        name = self.nameEntry.get()
+        password = self.passwordEntry.get()
+        if self.encrypt_password(website, name, password):
+            view4 = tk.Tk()
+            view4.title("Success")
+            tk.Label(view4, text="You have successfully added a new password", font="bold").grid(row=0, column=0)
+            self.write_main_window()
+            view4.mainloop()
+
+
+    def encrypt_password(self, website, name, password):
+        key = Fernet.generate_key()
+        passwordEncryptor = Fernet(key)
+        encryptedPassword = passwordEncryptor.encrypt(password.encode())
+        keyEncryptor = Fernet(master_key)
+        encryptedKey = keyEncryptor.encrypt(key)
+        f1 = open("passwords.txt", "a")
+        f1.write(website + "," + name + f",{encryptedPassword}\n")
+        f1.close()
+        f2 = open("keys.txt", "a")
+        f2.write(f"{encryptedKey}\n")
+        f2.close()
+        return True
+
+    def create_user(self, name, password):
+        salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
+        hashedPassword = hashlib.pbkdf2_hmac(
+            'sha256',
+            password.encode('utf-8'),
+            salt,
+            100000,
+        )
+        hashedPassword = binascii.hexlify(hashedPassword)
+        storage = salt + hashedPassword
+        f1 = open("user.txt", "w")
+        f1.write(name + f",{storage}")
+        f1.close()
 
 app = passwordApp()
 app.mainloop()
 
-
-"""
-def create_user(name, password):
-    salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
-    hashedPassword = hashlib.pbkdf2_hmac(
-        'sha256',
-        password.encode('utf-8'),
-        salt,
-        100000,
-    )
-    hashedPassword = binascii.hexlify(hashedPassword)
-    storage = salt + hashedPassword
-    f1 = open("user.txt", "w")
-    f1.write(name + f",{storage}")
-    f1.close()
-
-def encrypt_password(website, name, password):
-    key = Fernet.generate_key()
-    passwordEncryptor = Fernet(key)
-    encryptedPassword = passwordEncryptor.encrypt(password.encode())
-    keyEncryptor = Fernet(master_key)
-    encryptedKey = keyEncryptor.encrypt(key)
-    f1 = open("passwords.txt", "a")
-    f1.write(website+","+name+f",{encryptedPassword}\n")
-    f1.close()
-    f2 = open("keys.txt", "a")
-    f2.write(f"{encryptedKey}\n")
-    f2.close()
-    return True
-
-
-def decrypt_password(number):
-    encryptedPassword = ""
-    encryptedKey = ""
-    f1 = open("passwords.txt", "r")
-    f2 = open("keys.txt", "r")
-    lines = f1.readlines()
-    i = 0
-    for line in lines:
-        if i == number:
-            parts = line.split(",")
-            end = len(parts[2]) - 2
-            encryptedPassword = parts[2][2:end]
-        i += 1
-    lines = f2.readlines()
-    i = 0
-    for line in lines:
-        if i == number:
-            end = line.__len__() - 2
-            encryptedKey = line[2:end]
-        i += 1
-    keyDecryptor = Fernet(master_key)
-    key = keyDecryptor.decrypt(encryptedKey.encode())
-    passwordDecryptor = Fernet(key)
-    password = passwordDecryptor.decrypt(encryptedPassword.encode()).decode()
-    return password
-
-
-def show_password(number):
-    password = decrypt_password(number)
-    view3 = Tk()
-    view3.title("Password")
-    Label(view3, text=password).grid(row=0, column=0)
-    view3.mainloop()
-
-
-def password_confirmation(websiteEntry, nameEntry, passwordEntry):
-    website = websiteEntry.get()
-    name = nameEntry.get()
-    password = passwordEntry.get()
-    if encrypt_password(website, name, password):
-        view4 = Tk()
-        view4.title("Success")
-        Label(view4, text="You have successfully added a new password", font="bold").grid(row=0, column=0)
-        view4.mainloop()
-        submit()
-
-
-def add_password():
-    view3 = Tk()
-    view3.title("Add Password")
-    Label(view3, text="Website:").grid(row=0, column=0)
-    Label(view3, text="UserName:").grid(row=1, column=0)
-    Label(view3, text="Password:").grid(row=2, column=0)
-    tkinter.Entry(view3, textvariable=websiteEntry).grid(row=0, column=1)
-    tkinter.Entry(view3, textvariable=nameEntry).grid(row=1, column=1)
-    tkinter.Entry(view3, textvariable=passwordEntry).grid(row=2, column=1)
-    Button(view3, text='confirm', bd='5', command=lambda: password_confirmation(websiteEntry, nameEntry, passwordEntry)).grid(row=3, column=1)
-    view3.mainloop()
-"""
 
 
