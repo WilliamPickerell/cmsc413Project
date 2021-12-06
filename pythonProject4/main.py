fa4f17c04c8da82f35896a82136ff98a6b8934aa
WilliamPickerell <66374031+WilliamPickerell@users.noreply.github.com>
2021-12-05 19:50:45 -0500

    added more features

--- pythonProject4/main.py
+++ pythonProject4/main.py
@@ -1,5 +1,47 @@
 from cryptography.fernet import Fernet
+import os
+import hashlib
+import binascii
 master_key = b'Cp1hH7cSCOO1hpp5yQx3kPDh7rQ_4VdFjoTp1GuyH_c='
+
+
+def create_user(name, password):
+    salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
+    hashedPassword = hashlib.pbkdf2_hmac(
+        'sha256',
+        password.encode('utf-8'),
+        salt,
+        100000,
+    )
+    hashedPassword = binascii.hexlify(hashedPassword)
+    storage = salt + hashedPassword
+    f1 = open("user.txt", "w")
+    f1.write(name + f",{storage}")
+    f1.close()
+
+
+def login_user(name, password):
+    f1 = open("user.txt", "r")
+    lines = f1.readlines()
+    for line in lines:
+        info = line.split(",")
+        if info[0] == name:
+            end = len(info[1]) - 1
+            salt = info[1][2:66]
+            key = info[1][66:end]
+            new_key = hashlib.pbkdf2_hmac(
+                'sha256',
+                password.encode('utf-8'),
+                salt.encode('ascii'),
+                100000
+            )
+            new_key = binascii.hexlify(new_key).decode('ascii')
+            if new_key == key:
+                return True
+            else:
+                return False
+        else:
+            return False
 
 
 def encrypt_password(name, password):
@@ -46,8 +88,9 @@ def decrypt_password(number):
 
 
 if __name__ == '__main__':
-    encrypt_password("test", "test1")
-    pword = decrypt_password(1)
-    print(pword)
+    #encrypt_password("test", "test1")
+    #pword = decrypt_password(1)
+    #create_user("William", "password123")
+    print(login_user("William", "password123"))
+    #print(pword)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
